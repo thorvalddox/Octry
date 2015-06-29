7c8a01e650c32bd6baa21a38b7e359cc883b00eb
thorvalddox <thorvalddx94@gmail.com>
2015-06-29 13:57:49 +0200

    Added heightmap

--- Octry/make_map.py
+++ Octry/make_map.py
@@ -1,11 +1,22 @@
-map = [[float(n) for n in l.split("\t") for l in open("worldgen.txt").split("\n")
-h = np.array(map)
+import numpy as np
+from PIL import Image
 
-h.rescale(0,255)
-image=np.zeros(tuple(h.height.shape) + (3,),dtype=np.uint8)
-image[:,:,0] = ((h.height)%15 > 12)*255
-image[:,:,1] = h.height
-image[:,:,2] = 255-h.height
+map = [[float(n) for n in l.split("\t") if not n.isspace() and n] for l in open("worldgen.txt").read().split("\n")if  not l.isspace() and l]
+height = np.array(map)
+
+print(height)
+def rescale(arr,newmin,newmax):
+    oldmin = np.amin(np.amin(arr))
+    oldmax = np.amin(np.amax(arr))
+    factor = (newmax-newmin)/(oldmax-oldmin)
+    return factor*(arr-oldmin)+newmin
+    
+    
+height = rescale(height,0,255)
+image=np.zeros(tuple(height.shape) + (3,),dtype=np.uint8)
+image[:,:,0] = ((height)%15 > 12)*255
+image[:,:,1] = height
+image[:,:,2] = 255-height
 
 im = Image.fromarray(image)
 im.save("heightmap.png")
