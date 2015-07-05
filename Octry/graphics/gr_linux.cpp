357445e852f6f5f677b65ebda3a46b9450685199
Gerwin <gerwin.dox@ugent.be>
2015-07-05 11:03:01 +0200

    Some renaming

---
+++ Octry/graphics/gr_linux.cpp
@@ -0,0 +1,85 @@
+// -- Written in C -- //
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<X11/X.h>
+#include<X11/Xlib.h>
+#include<GL/gl.h>
+#include<GL/glx.h>
+
+int gui_init();
+int gui_draw();
+int gui_close();
+
+Display                 *dpy;
+Window                  root;
+GLint                   att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
+XVisualInfo             *vi;
+Colormap                cmap;
+XSetWindowAttributes    swa;
+Window                  win;
+GLXContext              glc;
+XWindowAttributes       gwa;
+XEvent                  xev;
+
+
+
+int main(int argc, char *argv[]) {
+
+    dpy = XOpenDisplay(NULL);
+
+    if(dpy == NULL) {
+        printf("\n\tcannot connect to X server\n\n");
+        exit(0);
+    }
+
+    root = DefaultRootWindow(dpy);
+
+    vi = glXChooseVisual(dpy, 0, att);
+
+    if(vi == NULL) {
+        printf("\n\tno appropriate visual found\n\n");
+        exit(0);
+    } 
+    else {
+        printf("\n\tvisual %p selected\n", (void *)vi->visualid); /* %p creates hexadecimal output like in glxinfo */
+    }
+
+
+    cmap = XCreateColormap(dpy, root, vi->visual, AllocNone);
+
+    swa.colormap = cmap;
+    swa.event_mask = ExposureMask | KeyPressMask;
+
+    win = XCreateWindow(dpy, root, 0, 0, 600, 600, 0, vi->depth, InputOutput, vi->visual, CWColormap | CWEventMask, &swa);
+
+    XMapWindow(dpy, win);
+    XStoreName(dpy, win, "VERY SIMPLE APPLICATION");
+
+    glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
+    glXMakeCurrent(dpy, win, glc);
+
+    gui_init();
+
+    while(1) {
+        XNextEvent(dpy, &xev);
+        fprintf (stderr, "Next event\n");
+        if(xev.type == Expose) {
+            XGetWindowAttributes(dpy, win, &gwa);
+            glViewport(0, 0, gwa.width, gwa.height);
+            gui_draw();
+            glXSwapBuffers(dpy, win);
+            fprintf (stderr, "Exposed: %i\n", glGetError());
+        }
+
+        else if(xev.type == KeyPress) {
+            fprintf (stderr, "Pressed\n");
+            glXMakeCurrent(dpy, None, NULL);
+            gui_close();
+            glXDestroyContext(dpy, glc);
+            XDestroyWindow(dpy, win);
+            XCloseDisplay(dpy);
+            exit(0);
+        }
+    } 
+} 
---
+++ Octry/testgraph.cpp
@@ -0,0 +1,28 @@
+#include "GL/gl.h"
+#include "GL/glx.h"
+
+void DrawAQuad() {
+ glClearColor(1.0, 1.0, 1.0, 1.0);
+ glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+ glBegin(GL_QUADS);
+  glColor3f(1., 0., 0.); glVertex3f(-.75, -.75, 0.);
+  glColor3f(0., 1., 0.); glVertex3f( .75, -.75, 0.);
+  glColor3f(0., 0., 1.); glVertex3f( .75,  .75, 0.);
+  glColor3f(1., 1., 0.); glVertex3f(-.75,  .75, 0.);
+ glEnd();
+glFlush();
+} 
+
+int gui_init()
+{
+	glEnable(GL_DEPTH_TEST); 
+}
+int gui_draw()
+{
+	DrawAQuad();
+}
+int gui_close()
+{
+
+}
